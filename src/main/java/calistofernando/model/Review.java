8eea5b7e2a60310310888283c3aa201c978dc392
Fernando Calisto <fernandocalisto27@gmail.com>
2026-02-02 13:17:51 -0300

    feat: create Book and Review model classes

--- src/main/java/calistofernando/model/Review.java
+++ src/main/java/calistofernando/model/Review.java
@@ -1,4 +1,41 @@
 package calistofernando.model;
 
 public class Review {
+
+    private String username;
+    private int stars;
+    private String comment;
+
+    public Review(String username, int stars, String comment) {
+        this.username = username;
+        this.stars = stars;
+        this.comment = comment;
+    }
+
+    public Review() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
