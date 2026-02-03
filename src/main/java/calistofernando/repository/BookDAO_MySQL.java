5efd6cd2d0501fea67df49515db7d5f2b44c19ca
Fernando Calisto <fernandocalisto27@gmail.com>
2026-02-03 11:34:23 -0300

    feat: implement getBook method using try-with-resources

--- src/main/java/calistofernando/repository/BookDAO_MySQL.java
+++ src/main/java/calistofernando/repository/BookDAO_MySQL.java
@@ -5,6 +5,7 @@ import calistofernando.model.Book;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -50,6 +51,23 @@ public class BookDAO_MySQL implements BookDAO {
 
     @Override
     public Book getBook(int id) {
+
+        String command = "SELECT * FROM livros WHERE id = ?";
+
+        try (Connection c = ConnectionFactory.getConnection();
+             PreparedStatement stmt = c.prepareStatement(command)){
+
+            stmt.setInt(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Book(rs.getString("titulo"), rs.getString("autor"));
+                }
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao Achar o livro: " + e.getMessage());
+        }
         return null;
     }
 
