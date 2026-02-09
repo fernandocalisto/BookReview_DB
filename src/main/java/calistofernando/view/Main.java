e29f164bdab6a6f760b7e750e5f84d680419b567
Fernando Calisto <fernandocalisto27@gmail.com>
2026-02-09 11:31:54 -0300

    feat: initialize database and start app execution loop in Main

--- src/main/java/calistofernando/view/Main.java
+++ src/main/java/calistofernando/view/Main.java
@@ -5,5 +5,7 @@ import calistofernando.factory.CreateDatabase;
 public class Main {
     public static void main(String[] args) {
         new CreateDatabase().createDatabase();
+        App app = new App();
+        app.run();
     }
 }
